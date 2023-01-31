cced2f40c1c29361048d0e0a7759034f498a70b0
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-31 07:28:01 +0100

    Functions as Data _ 2

--- Higher_Order_Functions.js
+++ Higher_Order_Functions.js
@@ -1,3 +1,10 @@
+/* MEMO
+A higher-order function is a function that either accepts functions as parameters,
+returns a function, or both! We call functions that get passed in as parameters
+callback functions. Callback functions get invoked during the execution of the
+higher-order function.
+*/
+
 //  ### ### --- Functions as Data --- ### ###
 const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   for(let i = 1; i <= 1000000; i++) {
@@ -8,3 +15,23 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
 };
 
 // Write your code below
+
+
+//  ### ### --- anonymous function (a function without a name) --- ### ###
+higherOrderFunc(() => {
+  for (let i = 0; i <= 10; i++){
+    console.log(i);
+  }
+});
+
+// ### ### --- exercise 1 --- ### ###
+const addTwo = (num) => {
+  return num + 2;
+};
+
+const checkConsistentOutput = (func, val) => {
+  const checkA = val + 2;
+  const checkB = func(val);
+  return checkA === checkB ? func(val) : "inconsistent results";
+};
+console.log(checkConsistentOutput(addTwo, 34))
